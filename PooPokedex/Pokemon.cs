59ea8c52831bd0955a2bed97c30de85a59842165
thales <thalestr@outlook.com>
2023-01-24 16:13:23 -0300

    first commit

---
+++ PooPokedex/Pokemon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace PooPokedex
+{
+	public class Pokemon
+	{
+		public Pokemon()
+		{
+			this.Name = "";
+			this.Description = "";
+		}
+
+		private string name;
+
+		public string Name {
+			get
+			{
+				return this.name;
+			}
+			set
+			{
+
+        this.name = value;
+			}
+		}
+
+    private string description;
+
+    public string Description
+    {
+      get
+      {
+        return this.description;
+      }
+      set
+      {
+        this.description = value;
+      }
+    }
+
+		public void ShowPokemonData()
+		{
+			Console.WriteLine("Nome do Pokémon: " + this.Name);
+      Console.WriteLine("Descricao do Pokémon: " + this.Description);
+    }
+  }
+}
+
---
+++ PooPokedex/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooPokedex
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Pokemon p = new Pokemon();
+
+      p.Name = "Bulbasauro";
+      p.Description = "e um pokemon do tipo planta.";
+
+      p.ShowPokemonData();
+      Console.ReadKey();
+
+    }
+  }
+}
